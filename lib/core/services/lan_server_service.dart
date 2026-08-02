18a58ebb842c94d2eed6af3a1d7f518c9eeaa94a
Burhan <burhancengizbc@hotmail.com>
2026-08-02 11:20:25 +0300

    WebSocket imzasi duzeltildi ve g.dart dosyalari eklendi

--- lib/core/services/lan_server_service.dart
+++ lib/core/services/lan_server_service.dart
@@ -132,8 +132,8 @@ class LanServerService {
 
     // WebSocket: companion'lar (kurye) baglanir; sunucu olay yayinlar.
     final wsHandler = webSocketHandler((WebSocketChannel socket, String? protocol) {
-      _clients.add(socket);
-      socket.stream.listen(
+  _clients.add(socket);
+  socket.stream.listen(
         (data) {
           try {
             final msg = jsonDecode(data as String) as Map<String, dynamic>;
